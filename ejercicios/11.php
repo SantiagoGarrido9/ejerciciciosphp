1eb898615a6da66ef6037fdb6803599c5c742f69
Santiago Garrido <garrido.santiago.smr@gmail.com>
2020-10-05 16:57:07 +0100

    Ejercicio

--- ejercicios/11.php
+++ ejercicios/11.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Ejercicio 6</title>
-</head>
-<body>
-    <?php
-
-        
-    ?>
-</body>
-</html>
+<html><head>
+        <meta charset="UTF-8">
+        <title>Ejercicio 11</title>
+</head><body>
+    <h2>Formulario de libros</h2>
+    <form method="get" action="11action.php">
+        <label>Titulo</label><input type="text" value="" name="titulo"><br>
+        <label>Autor</label><input type="text" value="" name="autor"><br>
+        <label>Editorial</label><input type="text" value="" name="editorial"><br>
+        <label>Paginas</label><input type="text" value="" name="paginas"><br>
+        <input type="submit" value="enviar">
+    </form>
+</body></html>
--- ejercicios/12.php
+++ ejercicios/12.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Ejercicio 6</title>
-</head>
-<body>
-    <?php
-
-        
-    ?>
-</body>
-</html>
+<html><head>
+        <meta charset="UTF-8">
+        <title>Ejercicio 12</title>
+</head><body>
+    <h2>Enviar Campo Nombre</h2>
+    <form method="get" action="12action.php">
+        <label>Nombre</label><input type="text" name="nombre" required><br>
+        <input type="submit" value="enviar">
+    </form>
+</body></html>
