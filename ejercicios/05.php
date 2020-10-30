b35278c8e5b3f2c38529ab2bbd0112e449241004
Santiago Garrido <garrido.santiago.smr@gmail.com>
2020-10-30 02:50:38 +0000

    Subir bien los ejercicios

--- ejercicios/05.php
+++ ejercicios/05.php
@@ -1,13 +1,25 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ejercicio 5</title>
 </head>
 <body>
-    <?php
-
         
+    <?php
+        for($i=1 ; $i <= 169; $i++){
+            if(169 % $i != 0){
+                echo $i;
+                echo "<br>";
+            }
+            else {
+                echo "El numero" . $i  . " es primo";
+                echo "<br>";
+            }
+        } 
     ?>
+
 </body>
 </html>
